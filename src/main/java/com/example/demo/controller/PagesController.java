c0be20508ed8f8da36cacb2f5044b8381e08287a
themrjoe <vadimmanko666@gmail.com>
2022-03-26 23:47:31 +0200

    Implement cross origin for react app

--- src/main/java/com/example/demo/controller/PagesController.java
+++ src/main/java/com/example/demo/controller/PagesController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 @RequiredArgsConstructor
 public class PagesController {
