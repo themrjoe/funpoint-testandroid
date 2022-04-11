33a55987399803226f18d97492679baeb91913cd
themrjoe <vadimmanko666@gmail.com>
2022-04-11 16:46:40 +0300

    Fix deserializing problem

--- src/main/java/com/example/demo/entity/dto/FavouriteDto.java
+++ src/main/java/com/example/demo/entity/dto/FavouriteDto.java
@@ -1,8 +1,10 @@
 package com.example.demo.entity.dto;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class FavouriteDto {
 
     private int idEvent;
