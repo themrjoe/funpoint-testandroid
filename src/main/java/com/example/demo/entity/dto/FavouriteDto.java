df70d0d027515bf4b66827393767fb67945f6203
themrjoe <vadimmanko666@gmail.com>
2022-04-11 22:04:16 +0300

    Fix deserialize exception

--- src/main/java/com/example/demo/entity/dto/FavouriteDto.java
+++ src/main/java/com/example/demo/entity/dto/FavouriteDto.java
@@ -2,11 +2,13 @@ package com.example.demo.entity.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class FavouriteDto {
 
     private int idEvent;
