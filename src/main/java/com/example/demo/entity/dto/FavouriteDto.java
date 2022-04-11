ae3a9f2931060e3164f3df6f504d32a7bd4ae5d5
themrjoe <vadimmanko666@gmail.com>
2022-04-11 16:50:20 +0300

    Fix blyad deserialize

--- src/main/java/com/example/demo/entity/dto/FavouriteDto.java
+++ src/main/java/com/example/demo/entity/dto/FavouriteDto.java
@@ -1,10 +1,12 @@
 package com.example.demo.entity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
+@AllArgsConstructor
 public class FavouriteDto {
 
     private int idEvent;
