904c36f73af6f753b7cf193eceaffbd35bd6d19c
themrjoe <vadimmanko666@gmail.com>
2022-05-21 16:01:50 +0300

    Fix crossorigin

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://fun-point-app.herokuapp.com")
+@CrossOrigin(origins = "https://fun-point-app.herokuapp.com*")
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
